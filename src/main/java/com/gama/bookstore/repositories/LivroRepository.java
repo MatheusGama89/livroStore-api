ece1a4f33ec20165aa04a06b43b672387e7e77d3
MatheusGama89 <matheus_mgm@hotmail.com>
2021-03-11 16:07:29 -0300

    Repositories implementadas

---
+++ src/main/java/com/gama/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.gama.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.gama.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
