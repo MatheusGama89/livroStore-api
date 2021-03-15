3eaf5c452491e904790ce8dddf7b957bcd243e14
MatheusGama89 <matheus_mgm@hotmail.com>
2021-03-15 16:30:11 -0300

    atualizando o application.java

--- src/main/java/com/gama/bookstore/BookstoreApplication.java
+++ src/main/java/com/gama/bookstore/BookstoreApplication.java
@@ -16,9 +16,9 @@ import com.gama.bookstore.repositories.LivroRepository;
 public class BookstoreApplication implements CommandLineRunner{
 	
 	@Autowired
-	private CategoriaRepository categoriaRepositury;
+	private CategoriaRepository categoriaRepository;
 	@Autowired
-	private LivroRepository livroRepositury;
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
@@ -27,14 +27,21 @@ public class BookstoreApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "informatica", "livros");
-		
-		Livro l1 = new Livro(null, "Clean code", "Robert Martins","Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepositury.saveAll(Arrays.asList(cat1));
-		this.livroRepositury.saveAll(Arrays.asList(l1));
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Ficcção Científica", "Ficcção científica");
+		Categoria cat3 = new Categoria(null, "Biografias", "Livros de Biografias");
+				
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Engenharia de Software", "Louis V. Gerstner", "Lorem ipsum", cat1);
+		Livro l3 = new Livro(null, "The Time Machine", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l4 = new Livro(null, "The War of the Worlds", "H.G. Wells", "Lorem ipsum", cat2);
+		Livro l5 = new Livro(null, "I, Robot", "Isaac Asimov", "Lorem ipsum", cat2);
+				
+		cat1.getLivros().addAll(Arrays.asList(l1, l2));
+		cat2.getLivros().addAll(Arrays.asList(l3, l4, l5));
+				
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
 		
 		
 	}
