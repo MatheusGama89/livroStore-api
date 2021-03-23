6c4828dcb0a655c2e46127a298b7c2b0543650d0
MatheusGama89 <matheus_mgm@hotmail.com>
2021-03-23 15:38:17 -0300

    excecao personalizada para objeto não encontrado

--- src/main/java/com/gama/bookstore/services/CategoriaService.java
+++ src/main/java/com/gama/bookstore/services/CategoriaService.java
@@ -7,19 +7,20 @@ import org.springframework.stereotype.Service;
 
 import com.gama.bookstore.domain.Categoria;
 import com.gama.bookstore.repositories.CategoriaRepository;
+import com.gama.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		
-		return obj.orElse(null);
-		
-		
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado!: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 
 }
