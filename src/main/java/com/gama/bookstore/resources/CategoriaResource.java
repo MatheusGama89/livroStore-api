cee6501aef99b103b1ed22f51861f244e23d77e8
MatheusGama89 <matheus_mgm@hotmail.com>
2021-08-24 10:34:25 -0300

    Update de categorias 2

--- src/main/java/com/gama/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/gama/bookstore/resources/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.validation.constraintvalidation.SupportedValidationTarget;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,11 +53,16 @@ public class CategoriaResource {
 		
 	}
 	
-	@PutMapping
+	@PutMapping(value = "/{id}")
 	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody CategoriaDTO objDto){
 	Categoria newObj  = service.update(id, objDto);
 	
 	return ResponseEntity.ok().body(new CategoriaDTO(newObj));
 	
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete() {
+		
 	}
 }
